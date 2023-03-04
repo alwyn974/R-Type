0c9e5e347903fdd4f0a1042e12d7d37c3705c583
alwyn974 <alwyn974@gmail.com>
2023-03-04 23:05:08 +0400

    ➕ Added Cpack

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -2,19 +2,19 @@
 include(InstallRequiredSystemLibraries) # include any runtime libraries that are needed by the project for the current platform
 
 # Set some basic information about the project
-set(CPACK_PACKAGE_NAME "${PROJECT_NAME}")
+set(CPACK_PACKAGE_NAME "R-Type")
 set(CPACK_PACKAGE_CONTACT "Alwyn974")
 set(CPACK_PACKAGE_VENDOR "Alwyn974")
 set(CPACK_PACKAGE_DESCRIPTION "R-Type is a 2D space shooter game, in network mode, with a lot of different weapons and enemies.")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "R-Type is a 2D space shooter game, in network mode, with a lot of different weapons and enemies.")
 set(CPACK_PACKAGE_HOMEPAGE_URL "${PROJECT_HOMEPAGE_URL}")
 
-get_target_property(TARGET_TYPE ${PROJECT_NAME} TYPE)
+#[[get_target_property(TARGET_TYPE ${BINARY_NAME} TYPE)
 if (TARGET_TYPE STREQUAL STATIC_LIBRARY)
     set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${PROJECT_VERSION}-static-${CMAKE_SYSTEM_NAME}") # Static
 elseif (TARGET_TYPE STREQUAL SHARED_LIBRARY)
     set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${PROJECT_VERSION}-shared-${CMAKE_SYSTEM_NAME}") # Shared
-endif ()
+endif ()]]
 
 # Set the version information
 set(CPACK_PACKAGE_VERSION "${PROJECT_VERSION}")
@@ -23,46 +23,49 @@ set(CPACK_PACKAGE_VERSION_MINOR "${PROJECT_VERSION_MINOR}")
 set(CPACK_PACKAGE_VERSION_PATCH "${PROJECT_VERSION_PATCH}")
 
 # Set some resource files
-set(CPACK_RESOURCE_FILE_WELCOME "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
-set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE.txt")
-set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+set(CPACK_RESOURCE_FILE_WELCOME "${CMAKE_SOURCE_DIR}/README.md")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE.txt")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/README.md")
 
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "${CPACK_PACKAGE_NAME}") # Installation directory on the target system
-set(CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/assets/logo.ico") # Location of an icon to display during the installation process
+set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/assets/rtype/icons/logo.ico") # Location of an icon to display during the installation process
 set(CPACK_PACKAGE_CHECKSUM "SHA256") # Create a checksum for the package
 set(CPACK_MONOLITHIC_INSTALL OFF) # Install all components in one package, can be useful to force the installation of all components
 
 if (TARGET_TYPE STREQUAL "EXECUTABLE")
-    set(CPACK_PACKAGE_EXECUTABLES "${PROJECT_NAME}" "${PROJECT_NAME}") # Set the executable name
-    set(${PROJECT_NAME}_IS_EXECUTABLE ON)
+    set(CPACK_PACKAGE_EXECUTABLES
+            "r-type_server.exe" "R-Type Server"
+            "r-type_client.exe" "R-Type Client"
+    ) # Set the executables name
+    set(${BINARY_NAME}_IS_EXECUTABLE ON)
 endif ()
 
 # Components
 include(CPackComponent)
 cpack_add_install_type(Full DISPLAY_NAME "Everything")
-cpack_add_install_type(Developer DISPLAY_NAME "Development")
+#cpack_add_install_type(Developer DISPLAY_NAME "Development")
 cpack_add_install_type(Software DISPLAY_NAME "Software")
 
-cpack_add_component(headers
-        DISPLAY_NAME "Headers"
-        DESCRIPTION "C++ Header files"
-        GROUP "Development"
-        INSTALL_TYPES Developer Full
-)
-cpack_add_component(archive
-        DISPLAY_NAME "Archive"
-        DESCRIPTION "Static library"
-        GROUP "Development"
-        DEPENDS headers
-        INSTALL_TYPES Developer Full
-)
-cpack_add_component(cmake
-        DISPLAY_NAME "CMake"
-        DESCRIPTION "CMake files"
-        GROUP "Development"
-        DEPENDS headers archive
-        INSTALL_TYPES Developer Full
-)
+#cpack_add_component(headers
+#        DISPLAY_NAME "Headers"
+#        DESCRIPTION "C++ Header files"
+#        GROUP "Development"
+#        INSTALL_TYPES Developer Full
+#)
+#cpack_add_component(archive
+#        DISPLAY_NAME "Archive"
+#        DESCRIPTION "Static library"
+#        GROUP "Development"
+##        DEPENDS headers
+#        INSTALL_TYPES Full
+#)
+#cpack_add_component(cmake
+#        DISPLAY_NAME "CMake"
+#        DESCRIPTION "CMake files"
+#        GROUP "Development"
+#        DEPENDS headers archive
+#        INSTALL_TYPES Developer Full
+#)
 cpack_add_component(library
         DISPLAY_NAME "Library"
         DESCRIPTION "Shared library"
@@ -74,23 +77,28 @@ cpack_add_component(runtime
         DESCRIPTION "Runtime files"
         GROUP "Software"
         INSTALL_TYPES Full Software
-)
-cpack_add_component_group(Development
-        EXPANDED
-        DESCRIPTION "All of the tools you'll ever need to develop software"
+        REQUIRED
 )
 cpack_add_component_group(Software
         EXPANDED
         DESCRIPTION  "All of the tools you'll ever need to run software"
 )
+cpack_add_component_group(Development
+        EXPANDED
+        DESCRIPTION "All of the tools you'll ever need to develop software"
+)
+
+#get_cmake_property(CPACK_COMPONENTS_ALL COMPONENTS)
+#list(REMOVE_ITEM CPACK_COMPONENTS_ALL "devel" "headers" "cmake" "archive")
+set(CPACK_COMPONENTS_ALL "runtime" "Unspecified")
 
 # Source package generator
 set(CPACK_SOURCE_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${PROJECT_VERSION}-src")
 set(CPACK_SOURCE_GENERATOR "7Z;DEB;IFW;NSIS;NSIS64;NUGET;STGZ:TGZ;TXZ;ZIP")
-set(CPACK_SOURCE_IGNORE_FILES ".git;build;out;dist;cmake-*")
+set(CPACK_SOURCE_IGNORE_FILES ".git;build;out;dist;cmake-*;include;lib")
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/cpack/CPackDEB.cmake) # DEB
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/cpack/CPackNSIS.cmake) # NSIS
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/cpack/CPackRPM.cmake) # RPM
+include(${CMAKE_SOURCE_DIR}/cmake/cpack/CPackDEB.cmake) # DEB
+include(${CMAKE_SOURCE_DIR}/cmake/cpack/CPackNSIS.cmake) # NSIS
+include(${CMAKE_SOURCE_DIR}/cmake/cpack/CPackRPM.cmake) # RPM
 
 include(CPack)
--- cmake/cpack/CPackNSIS.cmake
+++ cmake/cpack/CPackNSIS.cmake
@@ -2,8 +2,8 @@
 ## NSIS
 ##
 
-set(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/assets\\\\logo.ico") # Installer logo
-set(CPACK_NSIS_MUI_UNIICON "${CMAKE_CURRENT_SOURCE_DIR}/assets\\\\logo.ico") # Uninstaller logo
+set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/assets/rtype/icons\\\\logo.ico") # Installer logo
+set(CPACK_NSIS_MUI_UNIICON "${CMAKE_SOURCE_DIR}/assets/rtype/icons\\\\logo.ico") # Uninstaller logo
 set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON) # Uninstall the previous version before installing the new one
 set(CPACK_NSIS_MODIFY_PATH ON) # Add the installation directory to the PATH environment variable
 set(CPACK_NSIS_DISPLAY_NAME "${CPACK_PACKAGE_NAME}") # Display name in the Add/Remove Programs control panel
@@ -12,21 +12,28 @@ set(CPACK_NSIS_HELP_LINK "${PROJECT_HOMEPAGE_URL}") # Link to the project's home
 set(CPACK_NSIS_URL_INFO_ABOUT "${PROJECT_HOMEPAGE_URL}") # Link to the project's homepage
 set(CPACK_NSIS_CONTACT "${CPACK_PACKAGE_CONTACT}") # Contact information
 set(CPACK_NSIS_EXECUTABLES_DIRECTORY "bin") # Modify where the executables should be, for the Start Menu, by default=bin
-if (${${PROJECT_NAME}_IS_EXECUTABLE})
-    set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${PROJECT_NAME}.exe") # Launch the application after the installation
+set(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\r-type_server.exe") # Modify the icon of the application in the Start Menu
+#[[if (${${BINARY_NAME}_IS_EXECUTABLE})
+    set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${BINARY_NAME}.exe") # Launch the application after the installation
     set(CPACK_NSIS_MUI_FINISHPAGE_RUN_NOTCHECKED ON) # Launch checkbox
     set(CPACK_NSIS_MUI_FINISHPAGE_RUN_PARAMETERS "") # Launch parameters
-endif ()
+endif ()]]
 set(CPACK_NSIS_MENU_LINKS
-        "https://alwyn974.github.io/${PROJECT_NAME}" "${PROJECT_NAME} Documentation" # Add a link to the documentation in the Start Menu
+        "https://alwyn974.github.io/R-Type" "R-Type Documentation" # Add a link to the documentation in the Start Menu
 )
 set(CPACK_NSIS_UNINSTALL_NAME "${CPACK_PACKAGE_NAME}-Uninstaller") # Name of the uninstaller
 set(CPACK_NSIS_WELCOME_TITLE "Welcome to ${CPACK_PACKAGE_NAME} ${PROJECT_VERSION}") # Title of the welcome page
 set(CPACK_NSIS_FINISH_TITLE "Thank you for installing ${CPACK_PACKAGE_NAME} ${PROJECT_VERSION}") # Title of the finish page
 
-set(CPACK_NSIS_MUI_HEADERIMAGE "${CMAKE_CURRENT_SOURCE_DIR}/assets\\\\nsis-header.bmp") # Header image
+set(CPACK_NSIS_MUI_HEADERIMAGE "${CMAKE_SOURCE_DIR}/assets/rtype/nsis\\\\nsis-header.bmp") # Header image
 set(CPACK_NSIS_MUI_HEADERIMAGE_NOSTRETCH ON) # Don't stretch the header image
-set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${CMAKE_CURRENT_SOURCE_DIR}/assets\\\\nsis-wizard.bmp") # Left image
+set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${CMAKE_SOURCE_DIR}/assets/rtype/nsis\\\\nsis-wizard.bmp") # Left image
 set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP_NOSTRETCH ON) # Don't stretch the left image
 set(CPACK_NSIS_BRANDING_TEXT "${CPACK_PACKAGE_NAME} ${PROJECT_VERSION}") # Text at the bottom of the install window
 set(CPACK_NSIS_IGNORE_LICENSE_PAGE OFF) # Disable the license page
+
+set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
+        Delete '$INSTDIR\\\\share'
+        Delete '$INSTDIR\\\\lib'
+        Delete '$INSTDIR\\\\include'
+")
