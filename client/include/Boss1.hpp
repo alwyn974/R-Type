79ee8760868881799a13e83b3dcf5efe085c978b
ChatDo <nicolas.julie@epitech.eu>
2023-03-05 22:12:19 +0400

    Server merged, have to add multi and fix grid size

---
+++ client/include/Boss1.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2023
+** Boss1.hpp
+** File description:
+** Boss1.hpp
+*/
+
+#ifndef R_TYPE_BOSS1_HPP
+#define R_TYPE_BOSS1_HPP
+
+#include "Enemy.hpp"
+
+class Boss : public Enemy {
+public:
+    explicit Boss(const std::string &uniqueName, uranus::ecs::component::Position pos, std::shared_ptr<engine::Texture> &texture);
+
+    void loop(size_t entity);
+
+    void colliding(const size_t &entity, const size_t &entityCollidingWith);
+
+    void animationCallback(size_t entity, const std::string &animationName);
+
+};
+
+#endif //R_TYPE_BOSS1_HPP
